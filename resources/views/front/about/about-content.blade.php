ea90daad2a0246d868289c10a7ab87db8c778f9d
Mahbubul Alam <mahbubulalam5676@gmail.com>
2017-10-09 01:14:28 +0600

    Project initialize with Laravel

---
+++ resources/views/front/about/about-content.blade.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mahbubul Alam
+ * Date: 10/9/2017
+ * Time: 12:14 AM
+ */
+?>
+@extends('front.master')
+@section('title')
+    This is about page to show about our website
+@endsection
+@section('content')
+    <h1>Welcome to About us</h1>
+@endsection
